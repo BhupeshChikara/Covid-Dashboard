bae1234d37db19479f90f38b0b6078a43ef23b73
BhupeshChikara <bhupesh.chikara@tothenew.com>
2020-04-14 20:58:59 +0530

    add twitter layout && carousel

--- src/app/dashboard/news/news.component.ts
+++ src/app/dashboard/news/news.component.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsComponent implements OnInit {
 
+  slideIndex = 2;
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+  this.showSlides(this.slideIndex);
   }
+
+ currentSlide(n) {
+  this.showSlides(this.slideIndex = n);
+ }
+
+ showSlides(n) {
+  var i=0;
+  console.log(n)
+  console.log(typeof n)
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  console.log(slides)
+  console.log(dots)
+  console.log(slides['style'])
+  if (n > slides.length) {this.slideIndex = 1}    
+  if (n < 1) {this.slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i]['style'].display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[this.slideIndex-1]['style'].display = "flex";  
+  dots[this.slideIndex-1].className += " active";
+}
 
 }
